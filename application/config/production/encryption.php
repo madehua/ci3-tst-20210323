900a1249dc031e320ff09ca0154bf7ece38236d9
dehua ma <1073969437@qq.com>
2021-03-24 19:22:02 +0800

    更改错误提示方式，规范注释方式

--- application/config/production/encryption.php
+++ application/config/production/encryption.php
@@ -1,7 +1,5 @@
 <?php
-/**
- * 生产环境必须要设置session加密字符串
- */
 $config['encryption_key'] = '';
+//生产环境必须要设置session加密字符串
 if (empty($config['encryption_key']))
-	throw new RuntimeException('encryption_key not set value');
+	show_error('encryption_key not set value', 500);
